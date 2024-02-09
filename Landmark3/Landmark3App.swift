a7989bb5a6948e525bafc2179394eb95b213fa87
Matthew Zhang <gzhang@lcs.on.ca>
2024-02-09 11:29:10 -0500

    Created detail view and organized project by creating Views group.

--- Landmark3/Landmark3App.swift
+++ Landmark3/Landmark3App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Landmark3App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DetailView()
         }
     }
 }
---
+++ Landmark3/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Landmark3
+//
+//  Created by GengYu Zhang on 2024-02-09.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
